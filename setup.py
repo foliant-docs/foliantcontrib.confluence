2b80cec6242bd673d54a469968bc6be18e1838cb
textexpert <116849615+textexpert@users.noreply.github.com>
2024-11-25 16:12:57 +0300

    Update setup.py to bump a new version 0.6.21
    Update setup.py to bump a new version 0.6.21

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
-    version='0.6.20',
+    version='0.6.21',
     author='Daniil Minukhin',
     author_email='ddddsa@gmail.com',
     url='https://github.com/foliant-docs/foliantcontrib.confluence',
